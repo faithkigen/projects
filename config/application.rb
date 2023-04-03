b798c0f7e7e20fff19736e3e6a70a62fada9fbd1
faithkigen <kigenfaith20@gmail.com>
2023-04-03 06:07:35 +0300

    added changes

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Projects
@@ -11,27 +9,14 @@ module Projects
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # config/application.rb
-
-
-    # ... other configuration options ...
-    config.session_store :cookie_store, key: '_interslice_session'
-
+    # CORS configuration
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
       end
     end
+
+    # Other configuration options...
   end
 end
-
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-  
